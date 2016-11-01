497e4200af66784ed1e9e587d3ff7fcd491a212b
Your Name <you@example.com>
2016-11-01 17:42:22 +0800

    butternife和L的添加、登录和注册的点击事件

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,8 +1,11 @@
 package cn.ucai.superwechat.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.utils.MFGT;
 import cn.ucar.superwechat.R;
 
 public class GuideActivity extends BaseActivity {
@@ -11,5 +14,18 @@ public class GuideActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.bt_login, R.id.bt_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_login:
+                MFGT.gotologin(this);
+                break;
+            case R.id.bt_register:
+                MFGT.gotoregister(this);
+                break;
+        }
     }
 }
